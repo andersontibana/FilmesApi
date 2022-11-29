d5eb28af2a33496a05a3e95aa5126542983c973a
Anderson <ped2.nexsolar@gmail.com>
2022-11-29 00:24:12 -0400

    Configuranado o banco de UsuarioApi

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dto;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createUsuarioDto)
+        {
+            return Ok();
+        }
+    }
+}
